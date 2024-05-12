230d9e7a864312f272ec8b5351a066743de2a200
The coolest farley <surelyilikemen@gmail.com>
2024-05-12 09:38:52 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Farls-Xavier/UiLibrary/main/Library.lua"))()
 
+local Player = game.Players.LocalPlayer
+
 local Window = Library:Window({
 	Title = "Lucky Blocks"
 })
@@ -16,6 +18,41 @@ local Tabs = {
 }
 
 local MainStuff = {
+    ["Kill all"] = Tabs.Main:Button({
+        Text = "Attempt to kill all",
+        Callback = function()
+            if Player.Backpack:FindFirstChild("HexSpitter") then
+                Player.Backpack.HexSpitter.Parent = Player.Character
+            else
+                if game.Players.LocalPlayer.Character:FindFirstChild("HexSpitter") then
+                    local HexSpitter = game.Players.LocalPlayer.Character.HexSpitter
+                    local ServerControl = HexSpitter.Remotes.ServerControl
+                    for _ = 1,20 do
+                        for _, Child in next, game.Players:GetPlayers() do
+                            if Child ~= game.Players.LocalPlayer then
+                                ServerControl:InvokeServer('RayHit', {['Position'] = Child.Character.Head.Position, ["Hit"] = Child.Character.Head})
+                            end
+                        end
+                    end
+                end
+            end
+            local HexSpitter = game.Players.LocalPlayer.Character.HexSpitter
+            local ServerControl = HexSpitter.Remotes.ServerControl
+            for _ = 1,20 do
+                for _, Child in next, game.Players:GetPlayers() do
+                    if Child ~= game.Players.LocalPlayer then
+                        ServerControl:InvokeServer('RayHit', {['Position'] = Child.Character.Head.Position, ["Hit"] = Child.Character.Head})
+                    end
+                end
+            end
+        end
+    }),
+
+    ["Seperator"] = Tabs.Main:Label({
+        Text = "",
+        Weight = Enum.FontWeight.Medium
+    }),
+
     ["Give 1 Block"] = Tabs.Main:Button({
         Text = "Give 1 Block",
         Callback = function()
